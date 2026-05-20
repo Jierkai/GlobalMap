9ee57e19b40ca3d170ef27769bd30679b154faa7
Jikai <1589027512@qq.com>
2026-05-20 14:49:07 +0800

    chore: 清理废弃的示例、文档和 Vue 适配器代码

--- packages/adapter-vue/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { toRef, fromRef } from './bridge.js';
-export { default as CgxImageryLayer } from './components/CgxImageryLayer.vue';
-export { default as CgxTilesetLayer } from './components/CgxTilesetLayer.vue';
-export { default as CgxFeature } from './components/CgxFeature.vue';
-export { useLayer } from './hooks/useLayer';
-export { useFeature } from './hooks/useFeature';
