d48db7284192d808ead34fb5f32ed59c7c481d57
Jikai <1589027512@qq.com>
2026-07-23 18:03:28 +0800

    feat(core): task 24 core 公共出口收口（Map3D/EventBus/Base 类/13 Manager/全部 type/setCesiumBaseUrl）

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,19 @@
-export {}
+// @globalmap/core 公共出口（任务 24 统一收口）。
+// 各域目录的 index.ts 只导出公共 API，内部实现文件不导出（保持包边界）。
+export * from './type'
+export * from './map'
+export * from './layer'
+export * from './graphic'
+export * from './primitive'
+export * from './plot'
+export * from './measure'
+export * from './roam'
+export * from './effect'
+export * from './material'
+export * from './analyse'
+export * from './transform'
+export * from './control'
+export * from './resource'
+export * from './scene'
+export * from './event'
+export * from './util'
